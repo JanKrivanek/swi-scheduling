12a5fecb781244b3a16a7b05bf08f9ac8401a25a
Jan Krivanek <jan.krivanek@solarwinds.com>
2020-12-10 15:37:21 +0100

    code move

--- src/WorkInserter/Program.cs
+++ src/WorkInserter/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
@@ -9,7 +7,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using SolarWinds.ActivityContext.Abstractions;
 using SolarWinds.ActivityContext.AspNet;
-using SolarWinds.Messaging.Abstractions;
 using SolarWinds.SlingAuth.Client;
 using SolarWinds.SlingAuth.Client.AspNet;
 //using SolarWinds.SlingAuth.Client.GrpcCore;
@@ -75,92 +72,4 @@ namespace WorkInserter
         }
 
     }
-
-    public class TenantsProducer: BusUserBase
-    {
-        private readonly ITenantHelper tenantHelper;
-
-        public TenantsProducer(SchedulerQueueSettings settings, ILogger<TenantsProducer> logger, ITenantHelper tenantHelper) 
-            : base(settings, logger)
-        {
-            this.tenantHelper = tenantHelper;
-        }
-
-        protected override void OnStart(IMessageBus messageBus)
-        {
-            InsertWork().Wait();
-        }
-
-        private async Task InsertWork()
-        {
-            CancellationToken cancellationToken = CancellationToken.None;
-            var tenantIds = await tenantHelper.GetAllTenantIds(cancellationToken).ConfigureAwait(false);
-
-            await Task.WhenAll(tenantIds.Select(id => base.MessageBus.PublishAsync(Topic, id, cancellationToken)))
-                .ConfigureAwait(false);
-        }
-    }
-
-    //public class Test
-    //{
-    //    private ISlingAuthUser slingAuthUser;
-    //    private IActivityImpersonationProvider impersonationProvider;
-
-    //    public Test(ISlingAuthUser slingAuthUser, IActivityImpersonationProvider impersonationProvider)
-    //    {
-    //        this.slingAuthUser = slingAuthUser;
-    //        this.impersonationProvider = impersonationProvider;
-    //    }
-
-    //    public async Task DoWork(CancellationToken token)
-    //    {
-    //        await impersonationProvider.RunAsService(DoWorkInternal, ActivityConfiguration.Default, token);
-
-    //        await impersonationProvider.RunAsTenant(0, WhatToDoPerTenant,
-    //            ActivityConfiguration.Default,
-    //            token);
-    //    }
-
-    //    private async Task DoWorkInternal(CancellationToken token)
-    //    {
-    //        var tentInfos = await slingAuthUser.GetAllTenantsAsync(token);
-    //        foreach (var tenantInfo in tentInfos)
-    //        {
-    //            Console.WriteLine(tenantInfo.TenantId);
-
-    //            //await impersonationProvider.RunAsTenant(tenantInfo.TenantId, WhatToDoPerTenant,
-    //            //    ActivityConfiguration.Default,
-    //            //    token);
-
-    //        }
-    //    }
-
-    //    private async Task WhatToDoPerTenant(CancellationToken token)
-    //    {
-
-
-    //        Console.WriteLine("Current tenant Id from ambient context: {0}",
-    //            AuthenticationContext.Current.Slingshot.TenantId);
-    //    }
-
-    //    public void Foo()
-    //    {
-            
-    //    }
-    //}
-
-    //public class Consumer
-    //{
-    //    private IActivityImpersonationProvider impersonationProvider;
-
-    //    private void ConsumeNext(int tenantId, CancellationToken token)
-    //    {
-    //        impersonationProvider.RunAsTenant(tenantId, WhatToDo, ActivityConfiguration.Default, token);
-    //    }
-
-    //    private async Task WhatToDo(CancellationToken token)
-    //    {
-
-    //    }
-    //}
 }
---
+++ src/WorkInserter/TenantsProducer.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using SolarWinds.Messaging.Abstractions;
+using TenantHelper;
+
+namespace WorkInserter
+{
+    public class TenantsProducer: BusUserBase
+    {
+        private readonly ITenantHelper tenantHelper;
+
+        public TenantsProducer(SchedulerQueueSettings settings, ILogger<TenantsProducer> logger, ITenantHelper tenantHelper) 
+            : base(settings, logger)
+        {
+            this.tenantHelper = tenantHelper;
+        }
+
+        protected override void OnStart(IMessageBus messageBus)
+        {
+            InsertWork().Wait();
+        }
+
+        private async Task InsertWork()
+        {
+            CancellationToken cancellationToken = CancellationToken.None;
+            var tenantIds = await tenantHelper.GetAllTenantIds(cancellationToken).ConfigureAwait(false);
+
+            await Task.WhenAll(tenantIds.Select(id => base.MessageBus.PublishAsync<long>(Topic, id, cancellationToken)))
+                .ConfigureAwait(false);
+        }
+    }
+}
